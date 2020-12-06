755c4a9bb17e79da739155fae0f9ef79378cd586
Clément Albanel <57948666+calbanel@users.noreply.github.com>
2020-12-06 16:35:45 +0100

    Creation classe WorldFromFile.

---
+++ src/main/java/fr/ubx/poo/game/WorldFromFile.java
@@ -0,0 +1,15 @@
+package fr.ubx.poo.game;
+
+import static fr.ubx.poo.game.WorldEntity.*;
+
+public class WorldFromFile extends World{
+
+    public WorldFromFile(String file) {
+        super(fromFile(file));
+    }
+
+    private WorldEntity[][] fromFile(String file){
+        WorldEntity[][] we;
+        return null;
+    }
+}
